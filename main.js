9c422d3896c746c760e7863c7b0abf651a22d07c
ngonidzashewas <ngonidzashewas@gmail.com>
2024-09-09 23:45:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-// CHANGE NAV STYLE ON SCROLL//
+// nav shadow sort of when scroll
 window.addEventListener('scroll', () => { 
     document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);
+})
+
+//contact btn circular text rotate
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+    let text = textButton.querySelector('p');
+
+    text.innerHTML=text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
 
