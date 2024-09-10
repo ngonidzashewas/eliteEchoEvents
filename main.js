25723bf525e998c70d1e0ec542b0b3d6abd4866c
ngonidzashewas <ngonidzashewas@gmail.com>
2024-09-10 12:30:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,21 @@ textButtons.forEach(textButton => {
     text.innerHTML=text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
 })
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        599:{
+            slidesPerView: 2,
+            spaceBetween: 40
+        },
+        1023: {
+            slidesPerView: 3,
+            spaceBetween: 60
+        }
+    }
+});
