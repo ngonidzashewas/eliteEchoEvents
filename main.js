587315f902ff724b4acab3364f83cdb79bc553f2
ngonidzashewas <ngonidzashewas@gmail.com>
2024-09-16 15:37:42 +0200

    Lazy loading

--- main.js
+++ main.js
@@ -55,3 +55,30 @@ closeNavBtn.addEventListener('click', closeNav);
 nav.querySelectorAll('li a').forEach(navLink => {
     navLink.addEventListener('click', closeNav)
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img[data-src]"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.removeAttribute("data-src");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Fallback for browsers that don't support IntersectionObserver
+      lazyImages.forEach(function(lazyImage) {
+        lazyImage.src = lazyImage.dataset.src;
+        lazyImage.removeAttribute("data-src");
+      });
+    }
+  });
